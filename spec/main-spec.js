84dd660c24e4d8a1c09a3476c461ced6fbd1a504
ClemensSahs <clemens.sahs@slides-worker.org>
2020-10-05 18:37:08 +0200

    Add test config to test more cases with the same logic

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,6 +4,69 @@ const { sort } = require('../lib/css-declaration-sorter');
 
 describe('CSS Declaration Sorter', function () {
   let editor;
+  let testItem;
+  let testCase;
+  const testConfig = [
+    {
+      name: 'sorts CSS',
+      scopeName: 'source.css',
+      text: 'a{ flex: 0; box-sizing: border-box; border: 0; }',
+      expect: [
+        {
+          sortType: 'alphabetical',
+          expectString: 'a{ border: 0; box-sizing: border-box; flex: 0; }',
+        },
+        {
+          sortType: 'smacss',
+          expectString: 'a{ box-sizing: border-box; flex: 0; border: 0; }',
+        },
+        {
+          sortType: 'concentric-css',
+          expectString: 'a{ flex: 0; box-sizing: border-box; border: 0; }',
+        }
+      ]
+    },
+
+    {
+      name: 'sorts LESS',
+      scopeName: 'source.css.less',
+      text: 'a{\n//flex\nflex:0;\nbox-sizing:border-box;\nborder:0;\n}',
+      expect: [
+        {
+          sortType: 'alphabetical',
+          expectString: 'a{\nborder:0;\nbox-sizing:border-box;\n//flex\nflex:0;\n}',
+        },
+        {
+          sortType: 'smacss',
+          expectString: 'a{\nbox-sizing:border-box;\n//flex\nflex:0;\nborder:0;\n}',
+        },
+        {
+          sortType: 'concentric-css',
+          expectString: 'a{\n//flex\nflex:0;\nbox-sizing:border-box;\nborder:0;\n}',
+        }
+      ]
+    },
+
+    {
+      name: 'sorts SCSS',
+      scopeName: 'source.css.scss',
+      text: 'a{\n//flex\nflex:0;\nbox-sizing:border-box;\nborder:0;\n}',
+      expect: [
+        {
+          sortType: 'alphabetical',
+          expectString: 'a{\nborder:0;\nbox-sizing:border-box;\n//flex\nflex:0;\n}',
+        },
+        {
+          sortType: 'smacss',
+          expectString: 'a{\nbox-sizing:border-box;\n//flex\nflex:0;\nborder:0;\n}',
+        },
+        {
+          sortType: 'concentric-css',
+          expectString: 'a{\n//flex\nflex:0;\nbox-sizing:border-box;\nborder:0;\n}',
+        }
+      ]
+    }
+  ];
 
   beforeEach(function () {
     waitsForPromise(function () {
@@ -26,33 +89,26 @@ describe('CSS Declaration Sorter', function () {
     expect(editor.getText).toHaveBeenCalled();
   });
 
-  it('sorts CSS', function () {
-    editor.setGrammar(atom.grammars.grammarForScopeName('source.css'));
-    editor.setText('a{ flex: 0; border: 0; }');
-    waitsForPromise(() => sort('alphabetical', editor));
+  for (testItem of testConfig) {
+    const {
+      name: testName,
+      scopeName,
+      text
+    } = testItem;
+    for (testCase of testItem.expect) {
+      const {
+        sortType,
+        expectString
+      } = testCase;
 
-    editor.onDidChange(function () {
-      expect(editor.getText()).toBe('a{ border: 0; flex: 0; }');
-    });
-  });
+      it(`${testName}-${sortType} (${scopeName})`, function () {
+        editor.setGrammar(atom.grammars.grammarForScopeName(scopeName));
+        editor.setText(text);
 
-  it('sorts LESS', function () {
-    editor.setGrammar(atom.grammars.grammarForScopeName('source.css.less'));
-    editor.setText('a{\n//flex\nflex:0;\nborder:0;\n}');
-    waitsForPromise(() => sort('alphabetical', editor));
-
-    editor.onDidChange(function () {
-      expect(editor.getText()).toBe('a{\nborder:0;\n//flex\nflex:0;\n}');
-    });
-  });
-
-  it('sorts SCSS', function () {
-    editor.setGrammar(atom.grammars.grammarForScopeName('source.css.scss'));
-    editor.setText('a{\n//flex\nflex:0;\nborder:0;\n}');
-    waitsForPromise(() => sort('alphabetical', editor));
-
-    editor.onDidChange(function () {
-      expect(editor.getText()).toBe('a{\nborder:0;\n//flex\nflex:0;\n}');
-    });
-  });
+        return sort(sortType, editor).then(function () {
+          expect(editor.getText()).toBe(expectString);
+        });
+      });
+    }
+  }
 });
